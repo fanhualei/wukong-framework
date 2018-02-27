a9a80c386c8fae0a9a24c39b502d6994da5383b9
fanhualei <fanhl@189.cn>
2018-02-27 16:20:54 +0800

    添加了权限表

--- wukong-security/readme.md
+++ wukong-security/readme.md
@@ -4,13 +4,13 @@
 基础概念
 ---
 
-###autor2概念
+### autor2概念
 
     grant_type=password&username=johndoe&password=A3ddj3w
     grant_type=client_credentials
     grant_type=refresh_token
     
-###访问路径
+### 访问路径
 
     /oauth/2.0/authorize
     /oauth/2.0/token
@@ -19,7 +19,15 @@
     /oauth/jwt/login       
 
 
-###参考文件
+### 参考文件
 
     http://blog.csdn.net/u012373815/article/details/54633046
     http://wiki.jikexueyuan.com/project/spring-security/first-experience.html
+    
+### 撰写思路
+
+    jwt:见另外一个code的例子
+    spring认证，只用到user和role。需要添加UserDetailsService类，并在config中做注解
+    
+    
+    
--- wukong-security/src/main/java/com.wukong.security/service/UserService.java
+++ wukong-security/src/main/java/com.wukong.security/service/UserService.java
@@ -1,21 +1,34 @@
 package com.wukong.security.service;
 
+import com.wukong.security.dao.UserRoleMapper;
 import com.wukong.security.model.User;
 import com.wukong.security.dao.UserMapper;
+import static com.wukong.security.dao.UserDynamicSqlSupport.*;
+import static org.mybatis.dynamic.sql.SqlBuilder.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
-public class UserService {
+public class UserService implements UserDetailsService {
     //利用SpingIOC注入DAO变量
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private UserRoleMapper userRoleMapper;
+
+
 
     @Transactional
     public int deleteByPrimaryKey(Long userId_) {
@@ -57,4 +70,33 @@ public class UserService {
         return userMapper.selectByExample().build().execute();
     }
 
-}
+
+    //重写loadUserByUsername 方法获得 userdetails 类型用户
+    //https://github.com/527515025/springBoot/blob/master/springboot-SpringSecurity0/src/main/java/com/us/example/security/CustomUserService.java
+    @Override
+    public UserDetails loadUserByUsername(String username_) {
+
+
+        List<User> userList=  userMapper.selectDistinctByExample()
+                            .where(username,isEqualTo(username_))
+                            .build()
+                            .execute();
+
+
+        if(userList==null || userList.size()>0){
+            throw new UsernameNotFoundException("用户名不存在");
+        }
+
+        User user =userList.get(0);
+
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        //用于添加用户的权限。只要把用户权限添加到authorities 就万事大吉。
+
+
+
+
+
+        return null;
+    }
+
+    }
