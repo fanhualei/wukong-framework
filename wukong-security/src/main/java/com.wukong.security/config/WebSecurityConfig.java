fb414c9ca87510b837eac559a5832f0660f9e005
fanhualei <fanhl@189.cn>
2018-03-04 18:37:22 +0800

    保存了spring自身的配置文件

--- wukong-security/src/main/java/com.wukong.security/config/WebSecurityConfig.java
+++ wukong-security/src/main/java/com.wukong.security/config/WebSecurityConfig.java
@@ -1,15 +1,18 @@
 package com.wukong.security.config;
 
 import com.wukong.security.CustomUserDetailsService;
+import com.wukong.security.filter.JwtAuthenticationTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,20 +24,27 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    //返回自己的 UserDetailsService
-    UserDetailsService customUserService(){ return new CustomUserDetailsService(); }
+//    @Bean
+//    //返回自己的 UserDetailsService
+//    UserDetailsService customUserService(){ return new CustomUserDetailsService(); }
 
-    @Override
-    // 设置自己的UserDetailsService ，以及使用BCrypt进行密码的hash
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserService()).passwordEncoder(passwordEncoder());
-    }
+//    @Override
+//    // 设置自己的UserDetailsService ，以及使用BCrypt进行密码的hash
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService().passwordEncoder(passwordEncoder());
+//    }
 
-    @Bean
-    //装载BCrypt密码编码器
-    public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
+//    @Bean
+//    //装载BCrypt密码编码器
+//    public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
 
+//    @Bean
+//    @Override
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+
+    /* 下面这个配置，是没有jwt的情况下，只有spring security
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -43,5 +53,58 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().formLogin().defaultSuccessUrl("/hello").permitAll()//设置默认登录成功跳转页面
                 ;
     }
+    */
+
+
+
+    // jwt ------------------------------------------------------------------------
+    @Bean
+    public JwtAuthenticationTokenFilter authenticationTokenFilterBean() throws Exception {
+        return new JwtAuthenticationTokenFilter();
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // 由于使用的是JWT，我们这里不需要csrf
+                .csrf().disable()
+                // 基于token，所以不需要session
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests()
+                // 允许对于网站静态资源的无授权访问
+                .antMatchers(
+                        HttpMethod.GET,
+                        "/",
+                        "/*.html",
+                        "/favicon.ico",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js"
+                ).permitAll()
+                // 对于获取token的rest api要允许匿名访问
+                .antMatchers("/author/jwt/login").permitAll()
+                // 除上面外的所有请求全部需要鉴权认证
+                .anyRequest().authenticated();
+
+        // 添加JWT filter
+        httpSecurity.addFilterBefore(authenticationTokenFilterBean()
+                        , UsernamePasswordAuthenticationFilter.class);
+
+        // 禁用缓存
+        httpSecurity.headers().cacheControl();
+    }
+
+
+
+
+    // jwt end -------------------------------------------------------------------
+
+
+
+
+
+
+
 
 }
