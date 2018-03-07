0d220ffba9b1ebadbe9c8b9700e7b50e3188b0fb
fanhualei <fanhl@189.cn>
2018-03-07 18:11:40 +0800

    自己模块可以单独进行测试

---
+++ wukong-security/src/test/java/com/wukong/TestApplication.java
@@ -0,0 +1,22 @@
+package com.wukong;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+
+@SpringBootApplication()
+@EnableCaching
+public class TestApplication implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
