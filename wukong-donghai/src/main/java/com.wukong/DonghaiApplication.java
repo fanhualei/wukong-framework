23cf8f99a6483074a48b16df58d7d2fa61034f26
fanhualei <fanhl@189.cn>
2018-04-30 15:47:39 +0800

    添加了可以部署到tomcat中的代码

--- wukong-donghai/src/main/java/com.wukong/DonghaiApplication.java
+++ wukong-donghai/src/main/java/com.wukong/DonghaiApplication.java
@@ -3,18 +3,40 @@ package com.wukong;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication()
 @EnableCaching
-public class DonghaiApplication implements CommandLineRunner {
 
+// next is run debug
+
+//public class DonghaiApplication implements CommandLineRunner {
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(DonghaiApplication.class, args);
+//
+//    }
+//
+//}
+
+
+// next is run tomcat 下面的代码是为了部署到tomact下
+
+public class DonghaiApplication extends SpringBootServletInitializer {
     @Override
-    public void run(String... args) throws Exception {
-
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 设置启动类,用于独立tomcat运行的入口
+        return builder.sources(DonghaiApplication.class);
     }
-
 
     public static void main(String[] args) {
         SpringApplication.run(DonghaiApplication.class, args);
