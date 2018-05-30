11facf93da997a8beebd1a5e1626838823c21691
fanhualei <fanhl@189.cn>
2018-05-30 18:03:44 +0800

    服务器自动部署的脚本文件

--- wukong-donghai/src/main/java/com.wukong/controller/HelloController.java
+++ wukong-donghai/src/main/java/com.wukong/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping
     public String hello() {
-        return "Hello World 222";
+        return "Hello World 333";
     }
 }
