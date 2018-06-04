2756fbe07cdbd9fb30e85cebae4190295c267a10
fanhualei <fanhl@189.cn>
2018-06-04 14:08:52 +0800

    add 添加了对hello Controller的测试脚本

--- wukong-donghai/src/test/java/com/wukong/donghai/controller/HelloControllerTests.java
+++ wukong-donghai/src/test/java/com/wukong/donghai/controller/HelloControllerTests.java
@@ -1,4 +1,28 @@
 package com.wukong.donghai.controller;
 
-public class HelloControllerTests {
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerTests extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testHello() {
+        String url="/hello";
+
+        ResponseEntity<String> entity = this.restTemplate.getForEntity(url, String.class);
+        assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(entity.getBody()).isEqualTo("\"Hello World 你好\"");
+    }
 }
